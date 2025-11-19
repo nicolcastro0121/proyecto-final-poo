00bf5ffd05ec01ae777509de17830a53bedc37d1
nicolcastro0121 <20240757@aloe.ulima.edu.pe>
2025-11-19 18:43:00 -0500

    cambios en paquetes 1

---
+++ src/Clinica/Administrador.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clinica;
+
+import Empleados.*;
+
+/**
+ *
+ * @author N04109
+ */
+public class Administrador {
+    private String Code;
+
+}
+
