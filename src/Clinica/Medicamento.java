590fa1fb0feb329d2b40213bc3325b252355a346
Gromilale <fergmanbej@gmail.com>
2025-11-20 10:13:55 -0500

    Metods de la clase receta, guia UML

--- src/Clinica/Medicamento.java
+++ src/Clinica/Medicamento.java
@@ -1,13 +1,62 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Clinica;
 
-/**
- *
- * @author Nicol
- */
 public class Medicamento {
+    private double dosis;
+    private int frecuencia;
+    private int duracion;
+    private String nombre;
+    private String marca;
+
+    public Medicamento(double dosis, int frecuencia, int duracion, String nombre, String marca) {
+        this.dosis = dosis;
+        this.frecuencia = frecuencia;
+        this.duracion = duracion;
+        this.nombre = nombre;
+        this.marca = marca;
+    }
+    
+
+    public double getDosis() {
+        return dosis;
+    }
+
+    public void setDosis(double dosis) {
+        this.dosis = dosis;
+    }
+
+    public int getFrecuencia() {
+        return frecuencia;
+    }
+
+    public void setFrecuencia(int frecuencia) {
+        this.frecuencia = frecuencia;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    
+    
     
 }
--- src/Clinica/Receta.java
+++ src/Clinica/Receta.java
@@ -1,6 +1,73 @@
-
 package Clinica;
 
 public class Receta {
+
+    private Medicamento[] medicamentos;
+    private int cantidad;
+
+    public Receta() {
+        this.medicamentos = new Medicamento[10];
+        this.cantidad = 0;
+    }
+
+    public void agregar(Medicamento medicamento) {
+        if (cantidad < medicamentos.length) {
+            medicamentos[cantidad] = medicamento;
+            cantidad++;
+            System.out.println("Medicamento agregado: " + medicamento.getNombre());
+        } else {
+            System.out.println("No se pueden agregar más medicamentos");
+        }
+    }
     
+    public void eliminar(int posicion) {
+        if (posicion >= 0 && posicion < cantidad) {
+            System.out.println("Medicamento eliminado: " + medicamentos[posicion].getNombre());
+            for (int i = posicion; i < cantidad - 1; i++) {
+                medicamentos[i] = medicamentos[i + 1];
+            }
+            medicamentos[cantidad - 1] = null;
+            cantidad--;
+        } else {
+            System.out.println("Posición inválida");
+        }
+    }
+    public void modificar(int posicion, Medicamento nuevoMedicamento) {
+        if (posicion >= 0 && posicion < cantidad) {
+            medicamentos[posicion] = nuevoMedicamento;
+            System.out.println("Medicamento modificado en posición: " + posicion);
+        } else {
+            System.out.println("Posición inválida");
+        }
+    }
+     public void ver() {
+        System.out.println("=== RECETA MÉDICA ===");
+        System.out.println("Cantidad de medicamentos: " + cantidad);
+        for (int i = 0; i < cantidad; i++) {
+            Medicamento med = medicamentos[i];
+            System.out.println((i+1) + ". " + med.getNombre() + 
+                             " - " + med.getMarca() +
+                             " - Dosis: " + med.getDosis() +
+                             " - Frecuencia: " + med.getFrecuencia() + " veces/día" +
+                             " - Duración: " + med.getDuracion() + " días");
+        }
+    }
+
+    public Medicamento[] getMedicamentos() {
+        return medicamentos;
+    }
+
+    public void setMedicamentos(Medicamento[] medicamentos) {
+        this.medicamentos = medicamentos;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+     
+     
 }
