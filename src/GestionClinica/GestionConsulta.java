ed1101378742a5275f8b5ebb5510a7267e36998c
Gromilale <fergmanbej@gmail.com>
2025-11-20 16:35:46 -0500

    gestor consulta, metodos, lista

--- src/GestionClinica/GestionConsulta.java
+++ src/GestionClinica/GestionConsulta.java
@@ -1,13 +1,54 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package GestionClinica;
 
-/**
- *
- * @author Nicol
- */
+import Clinica.Consulta;
+
 public class GestionConsulta {
+    private Consulta[] consultas;
+    private int cantidad;
+    
+    public GestionConsulta() {
+        this.consultas = new Consulta[100];
+        this.cantidad = 0;
+    }
+    
+    public void agregar(Consulta consulta) {
+        if (cantidad < consultas.length) {
+            consultas[cantidad] = consulta;
+            cantidad++;
+            System.out.println("Consulta agregada para el paciente: " + consulta.getCita().getPaciente().getNombres());
+        } else {
+            System.out.println("No hay espacio para más consultas");
+        }
+    }
+    
+    public void modificar(int indice, Consulta nuevaConsulta) {
+        if (indice >= 0 && indice < cantidad) {
+            consultas[indice] = nuevaConsulta;
+            System.out.println("Consulta modificada en posición: " + indice);
+        } else {
+            System.out.println("Índice inválido");
+        }
+    }
+    
+    public Consulta buscar(int indice) {
+        if (indice >= 0 && indice < cantidad) {
+            System.out.println("Consulta encontrada: " + consultas[indice].getCita().getPaciente().getNombres());
+            return consultas[indice];
+        } else {
+            System.out.println("Consulta no encontrada en el índice: " + indice);
+            return null;
+        }
+    }
+
+    public void setConsultas(Consulta[] consultas) {
+        this.consultas = consultas;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+    
     
 }
