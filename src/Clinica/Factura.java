222d6292cde994c6bdf71b182a47d7dd7ad8381d
Leandro <Leandro@DESKTOP-K8HFD5R>
2025-11-20 19:00:21 -0500

    actualizacion de factura

--- src/Clinica/Factura.java
+++ src/Clinica/Factura.java
@@ -1,13 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Clinica;
 
-/**
- *
- * @author Nicol
- */
 public class Factura {
+    private int numero;
+    private String descripcion;
+
+    public Factura(int numero, String descripcion) {
+        this.numero = numero;
+        this.descripcion = descripcion;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
     
+    public void emitirFactura(){
+        System.out.println("Factura emitida:" + this.numero + "Descripcion:" + this.descripcion);
+    }
 }
