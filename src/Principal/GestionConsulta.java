315943231be0df9228c029675bf4a02bc05d4def
Leandro <Leandro@DESKTOP-K8HFD5R>
2025-11-23 01:17:31 -0500

    interfaz consulta,factura

---
+++ src/Principal/GestionConsulta.java
@@ -0,0 +1,295 @@
+
+package Principal;
+import Clinica.Consulta;
+
+
+public class GestionConsulta extends javax.swing.JFrame {
+    private datos_Consulta gestor; 
+    private Integer indiceModificar = null;
+    
+    public GestionConsulta(datos_Consulta gestor, Consulta cons, Integer indice) {
+        initComponents();
+        this.gestor = gestor;
+        this.indiceModificar = indice;
+        if (cons != null) { 
+            Motivo.setText(cons.getMotivo());
+            Precio.setText(String.valueOf(cons.getPrecio()));
+            estado.setText(cons.getEstado());
+            antecedentes.setText(cons.getAntecedentes());
+            signos_vitales.setText(cons.getSignosVitales());
+            examenes_fisicos.setText(cons.getExamenesFisicos());
+            diagnosticos.setText(cons.getDiagnosticos());
+            cant_operaciones.setText(String.valueOf(cons.getCantidadOrdenes()));
+            plan.setText(cons.getPlan());
+        }else{
+            Motivo.setText("");
+            Precio.setText("");
+            estado.setText("");
+            antecedentes.setText("");
+            signos_vitales.setText("");
+            examenes_fisicos.setText("");
+            diagnosticos.setText("");
+            cant_operaciones.setText("");
+            plan.setText("");
+        }
+    }
+    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(GestionConsulta.class.getName());
+
+    public GestionConsulta() {
+        initComponents();
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        aceptar = new javax.swing.JButton();
+        cancelar = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        Motivo = new javax.swing.JTextField();
+        Precio = new javax.swing.JTextField();
+        estado = new javax.swing.JTextField();
+        antecedentes = new javax.swing.JTextField();
+        signos_vitales = new javax.swing.JTextField();
+        examenes_fisicos = new javax.swing.JTextField();
+        diagnosticos = new javax.swing.JTextField();
+        cant_operaciones = new javax.swing.JTextField();
+        plan = new javax.swing.JTextField();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        aceptar.setText("aceptar");
+        aceptar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                aceptarActionPerformed(evt);
+            }
+        });
+
+        cancelar.setText("cancelar");
+        cancelar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cancelarActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setText("Motivo:");
+
+        jLabel2.setText("Precio:");
+
+        jLabel3.setText("Estado:");
+
+        jLabel4.setText("Antecedentes:");
+
+        jLabel5.setText("Signos Vitales:");
+
+        jLabel6.setText("Examenes Fisicos:");
+
+        jLabel7.setText("Diagnosticos:");
+
+        jLabel8.setText("Cantidad de Operaciones:");
+
+        jLabel9.setText("Plan Médico:");
+
+        Motivo.setText("jTextField1");
+
+        Precio.setText("jTextField2");
+
+        estado.setText("jTextField3");
+
+        antecedentes.setText("jTextField4");
+
+        signos_vitales.setText("jTextField5");
+
+        examenes_fisicos.setText("jTextField6");
+
+        diagnosticos.setText("jTextField7");
+
+        cant_operaciones.setText("jTextField8");
+
+        plan.setText("jTextField9");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(94, 94, 94)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(aceptar)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 146, Short.MAX_VALUE)
+                        .addComponent(cancelar)
+                        .addGap(119, 119, 119))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(jLabel8)
+                                .addComponent(jLabel7)
+                                .addComponent(jLabel6)
+                                .addComponent(jLabel5)
+                                .addComponent(jLabel4)
+                                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jLabel2)
+                                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 136, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(jLabel9))
+                        .addGap(58, 58, 58)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(plan, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(0, 0, Short.MAX_VALUE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(Motivo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(Precio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(estado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(antecedentes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(signos_vitales, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(examenes_fisicos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(diagnosticos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(cant_operaciones, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(64, 64, 64)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel1)
+                            .addComponent(Motivo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(25, 25, 25)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel2)
+                            .addComponent(Precio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(41, 41, 41)
+                        .addComponent(jLabel3))
+                    .addComponent(estado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(37, 37, 37)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(antecedentes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(35, 35, 35)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel5)
+                    .addComponent(signos_vitales, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(32, 32, 32)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel6)
+                    .addComponent(examenes_fisicos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(37, 37, 37)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel7)
+                    .addComponent(diagnosticos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(41, 41, 41)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel8)
+                    .addComponent(cant_operaciones, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(30, 30, 30)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel9)
+                    .addComponent(plan, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 63, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(aceptar)
+                    .addComponent(cancelar))
+                .addGap(42, 42, 42))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void aceptarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_aceptarActionPerformed
+        // TODO add your handling code here:
+        String pMotivo = Motivo.getText();
+        double pPrecio = Double.parseDouble(Precio.getText());
+        String pEstado = estado.getText();
+        String pAntecedentes = antecedentes.getText();
+        String pSignosVitales = signos_vitales.getText();
+        String pExamenesFisicos = examenes_fisicos.getText();
+        String pDiagnosticos = diagnosticos.getText();
+        int pCantidadOrdenes = Integer.parseInt(cant_operaciones.getText());
+        String pPlan = plan.getText();
+        
+        Consulta cons;
+        cons = new Consulta(pMotivo, pPrecio, pEstado, pAntecedentes,pSignosVitales, pExamenesFisicos, pDiagnosticos,pCantidadOrdenes, pPlan);
+        
+        if (indiceModificar != null) {
+            datos_consulta.listaConsulta.set(indiceModificar, cons);
+        } else {
+            datos_consulta.listaConsulta.add(cons);
+        }
+        
+        gestor.actualizarTabla();
+        this.dispose();
+        gestor.setVisible(true);
+    }//GEN-LAST:event_aceptarActionPerformed
+
+    private void cancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cancelarActionPerformed
+        // TODO add your handling code here:
+        this.dispose();
+        gestor.setVisible(true);
+    }//GEN-LAST:event_cancelarActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ReflectiveOperationException | javax.swing.UnsupportedLookAndFeelException ex) {
+            logger.log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new datos_Consulta().setVisible(true); 
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField Motivo;
+    private javax.swing.JTextField Precio;
+    private javax.swing.JButton aceptar;
+    private javax.swing.JTextField antecedentes;
+    private javax.swing.JButton cancelar;
+    private javax.swing.JTextField cant_operaciones;
+    private javax.swing.JTextField diagnosticos;
+    private javax.swing.JTextField estado;
+    private javax.swing.JTextField examenes_fisicos;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
+    private javax.swing.JTextField plan;
+    private javax.swing.JTextField signos_vitales;
+    // End of variables declaration//GEN-END:variables
+}
--- src/Principal/datos_Consulta.java
+++ src/Principal/datos_Consulta.java
@@ -1,20 +1,19 @@
 
 package Principal;
-import Clinica.Factura;
+import Clinica.Consulta;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
-import javax.swing.table.DefaultTableModel; 
+import javax.swing.table.DefaultTableModel;
 
-public class GestionFactura extends javax.swing.JFrame {
+public class datos_Consulta extends javax.swing.JFrame {
     
+    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(datos_Consulta.class.getName());
     
-    private static final java.util.logging.Logger logger =
-            java.util.logging.Logger.getLogger("Factura");
-    
-    public static ArrayList<Factura> listaFactura = new ArrayList<>();
-    
-    public GestionFactura() {
+    public static ArrayList<Consulta> listaConsulta = new ArrayList<>();
+
+    public datos_Consulta() {
         initComponents();
+        actualizarTabla();
     }
 
     
@@ -22,26 +21,13 @@ public class GestionFactura extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        Agregar = new javax.swing.JButton();
+        Modificar = new javax.swing.JButton();
+        Eliminar = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
-        Agregar = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        Eliminar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null, null, null},
-                {null, null, null},
-                {null, null, null},
-                {null, null, null}
-            },
-            new String [] {
-                "Numero", "Descripción", "cantidad"
-            }
-        ));
-        jScrollPane1.setViewportView(jTable1);
 
         Agregar.setText("Agregar");
         Agregar.addActionListener(new java.awt.event.ActionListener() {
@@ -50,10 +36,10 @@ public class GestionFactura extends javax.swing.JFrame {
             }
         });
 
-        jButton2.setText("Modificar");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        Modificar.setText("Modificar");
+        Modificar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                ModificarActionPerformed(evt);
             }
         });
 
@@ -64,6 +50,22 @@ public class GestionFactura extends javax.swing.JFrame {
             }
         });
 
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null, null}
+            },
+            new String [] {
+                "Motivo", "Precio", "Estado", "Antecedentes", "Signos Vitales", "Examenes Fisicos", "diagnosticos", "Cantidad de Operaciones", "Plan Médico"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
+        if (jTable1.getColumnModel().getColumnCount() > 0) {
+            jTable1.getColumnModel().getColumn(2).setResizable(false);
+        }
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -71,28 +73,28 @@ public class GestionFactura extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(64, 64, 64)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 452, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(139, 139, 139)
+                        .addGap(227, 227, 227)
                         .addComponent(Agregar)
-                        .addGap(50, 50, 50)
-                        .addComponent(jButton2)
-                        .addGap(64, 64, 64)
-                        .addComponent(Eliminar)))
-                .addContainerGap(76, Short.MAX_VALUE))
+                        .addGap(87, 87, 87)
+                        .addComponent(Modificar)
+                        .addGap(99, 99, 99)
+                        .addComponent(Eliminar))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(22, 22, 22)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 805, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(31, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(31, 31, 31)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 221, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(28, 28, 28)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 387, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 31, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(Agregar)
-                    .addComponent(jButton2)
+                    .addComponent(Modificar)
                     .addComponent(Eliminar))
-                .addContainerGap(17, Short.MAX_VALUE))
+                .addGap(34, 34, 34))
         );
 
         pack();
@@ -100,33 +102,31 @@ public class GestionFactura extends javax.swing.JFrame {
 
     private void AgregarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AgregarActionPerformed
         // TODO add your handling code here:
-        datos_Factura ventana = new datos_Factura();
-        ventana.setVisible(true);
-        this.setVisible(false);
+        
     }//GEN-LAST:event_AgregarActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void ModificarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ModificarActionPerformed
         // TODO add your handling code here:
         int fila = jTable1.getSelectedRow();
 
         if (fila == -1) {
-            JOptionPane.showMessageDialog(this, "Selecciona una factura para modificar.");
+            JOptionPane.showMessageDialog(this, "Selecciona un empleado para modificar.");
             return;
         }
 
-        Factura fac = listaFactura.get(fila);
+        Consulta cons = listaConsulta.get(fila);
 
-        datos_Factura ventana = new datos_Factura(this, fac, fila);
+        GestionConsulta ventana = new GestionConsulta(this, cons, fila);
         ventana.setVisible(true);
         this.setVisible(false);
-    }//GEN-LAST:event_jButton2ActionPerformed
+    }//GEN-LAST:event_ModificarActionPerformed
 
     private void EliminarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_EliminarActionPerformed
         // TODO add your handling code here:
         int filaSeleccionada = jTable1.getSelectedRow();
 
         if (filaSeleccionada == -1) {
-            JOptionPane.showMessageDialog(this, "Selecciona una factura para eliminar.");
+            JOptionPane.showMessageDialog(this, "Selecciona un empleado para eliminar.");
             return;
         }
         // Eliminar de la lista
@@ -139,11 +139,18 @@ public class GestionFactura extends javax.swing.JFrame {
         DefaultTableModel model = (DefaultTableModel) jTable1.getModel();
         model.setRowCount(0);
 
-        for (Factura fac : listaFactura) {
+        for (Consulta cons : listaConsulta) {
             model.addRow(new Object[]{
-                fac.getNumero(),
-                fac.getDescripcion(),
-                fac.getMonto(),
+                cons.getMotivo(),
+                cons.getPrecio(),
+                cons.getEstado(),
+                cons.getAntecedentes(),
+                cons.getSignosVitales(),
+                cons.getExamenesFisicos(),
+                cons.getExamenesFisicos(),
+                cons.getDiagnosticos(),
+                cons.getCantidadOrdenes(),
+                cons.getPlan(),
             });
         }
     }
@@ -176,13 +183,13 @@ public class GestionFactura extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> new GestionFactura().setVisible(true));
+        java.awt.EventQueue.invokeLater(() -> new datos_Consulta().setVisible(true));
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton Agregar;
     private javax.swing.JButton Eliminar;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JButton Modificar;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
